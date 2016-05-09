3102df814d23430edb63ec3bd9f2f1d259e2802f
Willy Tarreau <w@1wt.eu>
2016-05-09 14:03:07 +0200

    CLEANUP/MINOR: stats: fix accidental addition of member "env" in the applet ctx
    Commit 860c38b ("BUG/MEDIUM: stats: show backend may show an empty or
    incomplete result") accidently added an extra member in the struct that
    git-cherry-pick brought to 1.6 during the backport. Just remove it as
    it is not used here. This issue is 1.6-specific, no upstream patch exists.

--- include/types/applet.h
+++ include/types/applet.h
@@ -138,9 +138,6 @@ struct appctx {
 		struct {
 			struct proxy *px;	/* current proxy being dumped, NULL = not started yet. */
 		} be;				/* used by "show backends" command */
-		struct {
-			char **var;
-		} env;
 	} ctx;					/* used by stats I/O handlers to dump the stats */
 };
 
